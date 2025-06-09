7ef651bdf5eb0f4b730b8d30fc1e4efa2141a2bc
Ihor222 <miskivigor10@gmail.com>
2025-06-10 00:43:52 +0300

    fgfg

--- src/js/artists.js
+++ src/js/artists.js
@@ -10,7 +10,7 @@ const refs = {
 };
 
 function createArtistCard({ _id, strArtist, genres, strArtistThumb, strBiographyEN }) {
-  const genreTags = (genres || []).map(genre => `<span class="genre">${genre}</span>`).join('');
+  const genreTags = (genres || []).map(genre => `<span class="genre-tag">${genre}</span>`).join('');
   const shortBio = strBiographyEN?.length > 100 ? strBiographyEN.slice(0, 100) + '...' : (strBiographyEN || '');
 
   const imgSrc = strArtistThumb && typeof strArtistThumb === 'string' && strArtistThumb.trim() !== ''
@@ -19,10 +19,12 @@ function createArtistCard({ _id, strArtist, genres, strArtistThumb, strBiography
 
   return `
     <li class="artist-card" data-id="${_id}">
-      <img src="${imgSrc}" alt="${strArtist}" />
-      <div class="genres">${genreTags}</div>
+      <div class="artist-image-wrapper">
+        <img src="${imgSrc}" alt="${strArtist}" class="artist-image"/>
+      </div>
+      <div class="artist-genres">${genreTags}</div>
       <h3 class="artist-name">${strArtist}</h3>
-      <p class="description">${shortBio}</p>
+      <p class="artist-description">${shortBio}</p>
       <button class="learn-more" data-id="${_id}">
         Learn More
         <svg class="icon" width="24" height="24">
@@ -32,6 +34,7 @@ function createArtistCard({ _id, strArtist, genres, strArtistThumb, strBiography
     </li>
   `;
 }
+
 
 let page = 1;
 
